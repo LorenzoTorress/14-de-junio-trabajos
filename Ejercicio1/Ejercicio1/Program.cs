ea9d881267b7f45944bf592b533c5e8d15f7619c
Lorenzo <lorenzooliverio12@gmail.com>
2024-06-23 02:02:38 -0300

    Ejercicio1 completado

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<int> Enteros = new List<int>();
+        Enteros.Add(10);
+        Enteros.Add(20);
+        Enteros.Add(30);
+        Enteros.Add(40);
+        Enteros.Add(50);
+        Console.WriteLine(Enteros[0]);
+        Console.WriteLine(Enteros[4]);
+        Enteros.Remove(Enteros[2]);
+        Console.WriteLine("Escriba un numero a buscar");
+        int num = int.Parse(Console.ReadLine());
+        if (Enteros.Contains(num))
+        {
+            Console.WriteLine("El numero se encuentra en la lista");
+        }
+        else
+        {
+            Console.WriteLine("El numero no se encuentra en la lista");
+        }
+    }
+}
